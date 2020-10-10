caec2a748eb09c178d7689556bf0063c3b33a90f
Zayidu Ansari <zayidu11@gmail.com>
2020-10-10 13:49:32 +0400

    Implemented Main jumbotron

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import React from 'react';
-import jumboData from './fixtures/jumbo';
-import Jumbotron, { Container } from './components/jumbotron';
+import { JumbotronContainer } from './containers/jumbotron';
+
 export default function App() {
-  return (
-    <div className="App">
-      <Jumbotron.Container>
-        {jumboData.map((item) => (
-          <Jumbotron key={item.id} direction={item.direction} />
-        ))}
-      </Jumbotron.Container>
-    </div>
-  );
+  return <JumbotronContainer />;
 }
